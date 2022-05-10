0b22d19e482a814bccd04dba26f000aae37db42c
simonm <simon@carwiz.co.il>
2022-05-10 13:06:29 +0300

    tailwind set

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,16 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+  pure: ["./src/**/*.{vue,js,ts,jsx,tsx}", ". /index.html"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 }
