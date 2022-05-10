5aa4a334c218c3d2d1b27a327e2e22f32d6ddbf1
simonm <simon@carwiz.co.il>
2022-05-10 17:45:56 +0300

    styled set

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import './App.css'
 import React from "react";
 
 
+import { HomePage } from './app/containers/index';
+import styled from "styled-components";
+import tw from "twin.macro";
+
+
+const SomeDiv = styled.div`
+    width: 100px;
+    height: 100px;
+    ${tw`bg-black`}
+`;
+
 const App: React.FC = () => {
     return (
-        <div className="flex flex-col justify-center items-center h-screen">
-            Hello World !
-        </div>
 
+        <div className="h-full w-full w-full flex flex-col">
+            <SomeDiv>s</SomeDiv>
+            <HomePage></HomePage>
+        </div>
     );
 }
 
---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div className="flex flex-col w-full h-full items-center overflow-x-hidden">
+            Hello World !
+        </div>
+    )
+}
+
+export default HomePage;
