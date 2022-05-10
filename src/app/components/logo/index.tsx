d75ce554a5c0e902bfda7c60834f30adb8b5ea00
simonm <simon@carwiz.co.il>
2022-05-10 17:22:48 +0300

    comp

---
+++ src/app/components/logo/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import CarLogoImg from '/src/assets/images/car-logo.png';
+
+const logoTextTW = "text-xl md:text-2xl font-bold text-black m-1";
+
+const Logo: React.FC = () => {
+    return (
+        <div className="flex items-center">
+
+        </div>
+    )
+}
+
+export default Logo;
---
+++ src/app/components/navbar/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+    return (
+        <div className="
+            min-h-[68px]
+            w-full
+            max-w-2xl
+            flex
+            flex-row
+            items-center
+            lg:pl-12
+            lg:pr-12
+            justify-between
+            ">
+            Navbar
+        </div>
+    )
+}
+
+export default Navbar;
