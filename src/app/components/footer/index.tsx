b33fac46c4904bef0174551013c148ea5e4b20fb
simonm <simon@carwiz.co.il>
2022-05-19 15:14:07 +0300

    footer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -3,31 +3,48 @@ import styled from "styled-components";
 import tw from "twin.macro";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Logo} from "../index";
-import {faPhoneAlt} from "@fortawesome/free-solid-svg-icons";
+import {faEnvelope, faPhoneAlt} from "@fortawesome/free-solid-svg-icons";
 
 const FooterContainer = styled.div`
-  min-height: 24em;
+  //min-height: 24em;
   background-color: #1d2124;
   ${tw`
     flex
-    flex-wrap
+    flex-col
     min-w-full
-    // bg-gray-900
-    pt-7
-    md:pt-12
-    pb-1
+    pt-10
+    md:pt-16
     items-center
     justify-center
-  `}
+  `};
 `;
 
 const InnerContainer = styled.div`
   ${tw`
     flex
-    flex-wrap
     w-full
     h-full
     max-w-screen-2xl
+    flex-wrap
+  `};
+`;
+
+const BottomContainer = styled.div`
+  ${tw`
+    w-full
+    flex
+    max-w-screen-2xl
+    justify-center
+    md:justify-start
+    mt-7
+    md:mt-1
+  `};
+`;
+
+const CopyrightText = styled.small`
+  font-size: 12px;
+  ${tw`
+    text-gray-300
   `}
 `;
 
@@ -37,7 +54,11 @@ const AboutContainer = styled.div`
     flex-col
     mr-2
     md:mr-16
-  `}
+    pl-10
+    pr-10
+    md:pl-3
+    md:pr-3
+  `};
 `;
 
 const AboutText = styled.p`
@@ -48,16 +69,24 @@ const AboutText = styled.p`
     max-w-xs
     leading-5
     mt-2
-  `}
+  `};
 `;
 
 const SectionContainer = styled.div`
   ${tw`
+    w-full
+    md:w-auto
     flex
     flex-col
     mr-2
     md:mr-16
-  `}
+    pl-10
+    pr-10
+    md:pl-3
+    md:pr-3
+    mt-7
+    md:mt-0
+  `};
 `;
 
 const LinksList = styled.ul`
@@ -66,52 +95,54 @@ const LinksList = styled.ul`
     list-none
     flex
     flex-col
-  `}
+  `};
 `;
 
 const ListItem = styled.li`
   ${tw`
     mb-3
-  `}
+  `};
+
   & > a {
     ${tw`
       text-sm
       text-white
-      hover:text-gray-200
       transition-all
-    `}
+      hover:text-gray-200
+    `};
   }
-  
 `;
 
 const HeaderTitle = styled.h3`
   ${tw`
-    text-xl
-    md:text-2xl
+    text-2xl
     font-bold
     text-white
     mb-3
-  `}
-`;
-
-const RedIcon = styled.span`
-  ${tw`
-    w-5
-    h-5
-    rounded-full
-    flex
-    items-center
-    justify-center
-    text-white
-    text-base
-    mr-2
-  `}
+  `};
 `;
 
 const HorizontalContainer = styled.div`
   ${tw`
     flex
     items-center
+  `};
+`;
+
+const RedIcon = styled.span`
+  ${tw`
+    w-7
+    h-7
+    md:w-9
+    md:h-9
+    rounded-full
+    bg-red-500
+    flex
+    items-center
+    justify-center
+    text-white
+    text-base
+    mr-2
   `};
 `;
 
@@ -153,20 +184,34 @@ const Footer = (props: any) => {
                         <ListItem><a href="#">Terms &amp; Conditions</a></ListItem>
                     </LinksList>
                 </SectionContainer>
-                {/*SECTION 2*/}
+                {/*SECTION 3*/}
                 <SectionContainer>
                     <HeaderTitle>Call Now</HeaderTitle>
                     <HorizontalContainer>
                         <RedIcon>
                             <FontAwesomeIcon icon={faPhoneAlt} />
                         </RedIcon>
-                        <SmallText>+972542055028</SmallText>
+                        {/*todo: take number for env*/}
+                        <SmallText><a href="tel:0542055028">Call Us</a></SmallText>
                     </HorizontalContainer>
                 </SectionContainer>
-                4:32:37
-
-                Copyright &copy; {new Date().getFullYear()} Drive. All rights
-                reserved.
+                {/*SECTION 4*/}
+                <SectionContainer>
+                    <HeaderTitle>Mail</HeaderTitle>
+                    <HorizontalContainer>
+                        <RedIcon>
+                            <FontAwesomeIcon icon={faEnvelope} />
+                        </RedIcon>
+                        {/*todo: take email for env*/}
+                        <SmallText><a href="mailto:test@gmail.com">Email Us</a></SmallText>
+                    </HorizontalContainer>
+                </SectionContainer>
+                <BottomContainer>
+                    <CopyrightText>
+                        Copyright &copy; {new Date().getFullYear()} Drive. All rights
+                        reserved.
+                    </CopyrightText>
+                </BottomContainer>
             </InnerContainer>
         </FooterContainer>
     );
